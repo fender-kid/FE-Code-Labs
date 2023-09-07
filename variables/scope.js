1713b3b39d240459ce4a2e5e11841bb1614cd539
fender-kid <jtt.sides@gmail.com>
2023-09-07 17:27:41 -0500

    Testing scopes

--- variables/scope.js
+++ variables/scope.js
@@ -13,15 +13,26 @@
 
 // Exercise Two
 
-function studentInfo() {
-    let firstName = 'Bob';
-    let lastName = 'Jones';
-    let age = '31';
-    let country = 'England';
+// function studentInfo() {
+//     let firstName = 'Bob';
+//     let lastName = 'Jones';
+//     let age = '31';
+//     let country = 'England';
 
-    console.log(`${firstName} ${lastName} is ${age} years old and lives in ${country}.`); // using string interpolation    
+//     console.log(`${firstName} ${lastName} is ${age} years old and lives in ${country}.`); // using string interpolation    
+// }
+
+// firstName = 'James';
+
+// studentInfo();
+
+//Practice
+
+let x = 1;
+
+function func() {
+  let x = 2;
+  console.log(x);
 }
 
-firstName = 'James';
-
-studentInfo();
+func();
