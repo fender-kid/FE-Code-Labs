33f16865d92fb56539b312c567ae05438f2f9271
fender-kid <jtt.sides@gmail.com>
2023-09-07 12:36:01 -0500

    Added code for exercise 1 and 2

--- variables/scope.js
+++ variables/scope.js
@@ -1,12 +1,27 @@
-let firstName = 'John';
-let lastName = 'Smith';
-let age = '25';
+// let firstName = 'John';
+// let lastName = 'Smith';
+// let age = '25';
 
-// Exercise One
+// // Exercise One
 
-function studentInfo(firstName, lastName, age) {
-    console.log(firstName +' ' + lastName + ' is ' + age + ' years old.');  // using concatenation 
-    console.log(`${firstName} ${lastName} is ${age} years old.`);
+// function studentInfo(firstName, lastName, age) {
+//     console.log(firstName +' ' + lastName + ' is ' + age + ' years old.');  // using concatenation 
+//     console.log(`${firstName} ${lastName} is ${age} years old.`); // using string interpolation
+// }
+
+// studentInfo(firstName, lastName, age);
+
+// Exercise Two
+
+function studentInfo() {
+    let firstName = 'Bob';
+    let lastName = 'Jones';
+    let age = '31';
+    let country = 'England';
+
+    console.log(`${firstName} ${lastName} is ${age} years old and lives in ${country}.`); // using string interpolation    
 }
 
-studentInfo(firstName, lastName, age);
+firstName = 'James';
+
+studentInfo();
